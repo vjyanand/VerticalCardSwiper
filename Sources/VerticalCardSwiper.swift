ba2499210f6ddcf8d9c6a6e09f2a57f00a6cfddd
Vijay Anand <vijay@iavian.com>
2020-08-15 17:43:50 +0530

    Initial Commit

--- Sources/VerticalCardSwiper.swift
+++ Sources/VerticalCardSwiper.swift
@@ -261,7 +261,6 @@ extension VerticalCardSwiper: UICollectionViewDelegateFlowLayout {
 
     fileprivate func setupVerticalCardSwiperView() {
         verticalCardSwiperView = VerticalCardSwiperView(frame: self.frame, collectionViewLayout: flowLayout)
-        verticalCardSwiperView.decelerationRate = UIScrollView.DecelerationRate.fast
         verticalCardSwiperView.backgroundColor = UIColor.clear 
         verticalCardSwiperView.contentInsetAdjustmentBehavior = .never
         verticalCardSwiperView.showsVerticalScrollIndicator = false
